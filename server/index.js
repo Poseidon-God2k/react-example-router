c459f11ca91a470e24926b185a2dcc2023091cf0
quyenluc22082000 <ancucchocon@gmail.com>
2021-03-10 10:57:34 +0700

    Connect data and insert data from register page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {MongoClient} = require('mongodb');
+
+
+
+
+var Client = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/";
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,12 +23,34 @@ app.get('/', (req,res)=>{
 app.post("/api/login",(req, res)=>{
     const username = req.body.uname
     const password = req.body.pwd
-
-
-    console.log(username)
-    console.log(password)
 })
 
+
+app.post("/api/register",(req, res)=>{
+    const email = req.body.email
+    const password = req.body.pwd
+    const uname = req.body.uname
+    const birthday = req.body.birthday
+    const gender = req.body.gender
+    Client.connect(url, (err, db)=>{
+        if (err) throw err;
+        var shopDb = db.db("Shop_react");
+        var user = {email:email ,password:password,uname:uname,birthday:birthday,gender:gender}
+        shopDb.collection("account_db").insertOne(user, (err)=>{
+            if (err){
+                res.status(200).send({
+                    message: "Error register !!!"
+                })
+            };
+            res.status(200).send({
+                message: "Success register!!!!"
+            })
+            console.log("insert account: ",email);
+            
+            db.close();
+        })
+    })
+})
 app.listen(8888, () =>{
     console.log('running on port 8888');
 })
